99561296dbdf76d966083e03da0e4e700862e452
Cassio-Ares <cassiotadeu.ads@gmail.com>
2025-01-22 19:18:20 -0300

    atualization routes 2

--- src/utils/bcryptFunctions.js
+++ src/utils/bcryptFunctions.js
@@ -1,12 +1,12 @@
 import bcrypt from 'bcrypt';
 
-
-export const crypt = async (password) => {
+export const crypt = async password => {
   const saltRounds = parseInt(process.env.HASH_PASSWORD);
 
-  return await bcrypt.hash(password, saltRounds)
+  return await bcrypt.hash(password, saltRounds);
 };
 
 export const compareCrypt = async (password, passwordDB) => {
-    return await bcrypt.compare(password, passwordDB)
+  return await bcrypt.compare(password, passwordDB);
 };
+//TODO
